629af8d7aa0503e539960645edd004728ea332e8
root <root@DESKTOP-BLPH810>
2024-06-27 04:48:05 +0300

    push

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,10 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
@@ -10,9 +13,9 @@ class BaseModel:
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            id = Column(String(60), unique=True, nullable=False, primary_key=True)
+            created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+            updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
             storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
